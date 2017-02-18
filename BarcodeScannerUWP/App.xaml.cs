998f7a82eac5257f81e6b4f78303b87e1c82d0cc
Wm. Barrett Simms <barrett@wbsimms.com>
2017-02-18 13:41:18 -0600

    Release 3.0

--- BarcodeScannerUWP/App.xaml.cs
+++ BarcodeScannerUWP/App.xaml.cs
@@ -39,10 +39,10 @@ namespace BarcodeScannerUWP
 		{
 
 #if DEBUG
-            if (System.Diagnostics.Debugger.IsAttached)
-            {
-                DebugSettings.EnableFrameRateCounter = true;
-            }
+            //if (System.Diagnostics.Debugger.IsAttached)
+            //{
+            //    DebugSettings.EnableFrameRateCounter = true;
+            //}
 #endif
 
 			Frame rootFrame = Window.Current.Content as Frame;
